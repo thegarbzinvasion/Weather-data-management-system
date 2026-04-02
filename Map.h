f36b8171151f6763a2722a6644cf8eaeb86d4190
thegarbzinvasion <peteralisson123@gmail.com>
2026-04-02 17:30:20 +0800

    Added const_iterators to read weather data, monthly calculations, and sPcc computations

--- Map.h
+++ Map.h
@@ -11,14 +11,15 @@ template <class Key, class Value>
 class Map
 {
 public:
-    Map() = default;
     bool SearchPair(const Key& searchKeyValue) const;
     bool InsertPair(const Key& insertKeyValue, const Value& insertDefinitionValue);
-    size_t Size() const {return m_data.size();}
+    size_t Size() const { return m_data.size(); }
 
-    //using const_iterator = typename std::map<Key, Value>::const_iterator;
-    //const_iterator begin() const {return m_data.begin();}
-    //const_iterator end() const {return m_data.end();};
+    using const_iterator = class std::map<Key, Value>::const_iterator;
+    const_iterator begin() const { return m_data.begin(); }
+    const_iterator end() const { return m_data.end(); }
+
+    // I will need a const iterator to iterate through the vector and insert into the Map
 
 private:
     std::map<Key, Value> m_data;
@@ -27,22 +28,17 @@ private:
 // ---------------------------------------------------------
 //                      IMPLEMENTATION
 // ---------------------------------------------------------
+
 template <class Key, class Value>
-bool Map::SearchPair(const Key& searchKeyValue) const
+bool Map<Key, Value>::SearchPair(const Key& searchKeyValue) const
 {
     return m_data.find(searchKeyValue) != m_data.end();
 }
 
 template <class Key, class Value>
-bool Map::InsertPair(const Key& insertKeyValue, const Value& insertDefinitionValue) const
+bool Map<Key, Value>::InsertPair(const Key& insertKeyValue, const Value& insertDefinitionValue)
 {
     return m_data.insert({insertKeyValue, insertDefinitionValue}).second;
-}
-
-template <class Key, class Value>
-size_t Map::Size() const
-{
-    return m_data.size();
 }
 
 #endif // MAP_H
